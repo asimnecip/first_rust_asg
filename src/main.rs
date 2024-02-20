ff6e7d362a5edf1ef595a5fd6e1e7e7b2fd635b7
asimnecip <necipasimarslan@gmail.com>
2024-02-20 23:28:57 +0300

    assignment done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+enum Publication {
+    Book(Book),
+    Magazine(Magazine),
+}
+
+struct Book {
+    title: String,
+    author: String,
+    page_count: u32,
+}
+
+struct Magazine {
+    title: String,
+    issue: u32,
+    topic: String,
+}
+
+fn print_publications(publications: Vec<Publication>) {
+    for publication in publications {
+        match publication {
+            Publication::Book(ref book) => {
+                println!(
+                    "Kitap: {} yazar: {}, {} sayfa",
+                    book.title, book.author, book.page_count
+                );
+            }
+            Publication::Magazine(ref magazine) => {
+                println!(
+                    "Dergi: {} - Sayı: {}, Konu: {}",
+                    magazine.title, magazine.issue, magazine.topic
+                );
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let book_example = Book {
+        title: "Suç ve Ceza".to_string(),
+        author: "Fyodor Mihayloviç Dostoyevski".to_string(),
+        page_count: 600,
+    };
+
+    let magazine_example = Magazine {
+        title: "InterWebs".to_string(),
+        issue: 42,
+        topic: "New player in the Web3 world: kodzar".to_string(),
+    };
+
+    let publications = vec![
+        Publication::Book(book_example), 
+        Publication::Magazine(magazine_example)
+    ];
+
+    print_publications(publications);
 }
